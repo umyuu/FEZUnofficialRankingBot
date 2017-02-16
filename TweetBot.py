cc398d61b1a9c5c2478a3b36a67a50c7731a43db
umyuu <124dtiaka@gmail.com>
2017-02-16 18:28:12 +0900

    configparser
    configparser
    setting.ini

--- TweetBot.py
+++ TweetBot.py
@@ -1,5 +1,7 @@
 # pip install python-twitter
 # https://apps.twitter.com
+import configparser
+from logging import getLogger, StreamHandler, DEBUG
 import argparse
 from collections import OrderedDict
 from datetime import datetime
@@ -9,6 +11,16 @@ import shutil
 import requests
 import twitter
 
+config = configparser.ConfigParser()
+with open('./setting.ini', 'r', encoding='utf-8') as f:
+    config.read_file(f)
+# console output
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+
 class tweetbot():
     def __init__(self, args):
         self.args = args;
@@ -16,25 +28,30 @@ class tweetbot():
         self.upload = './upload/'
         self.images = './images/'
         self.dtNow = datetime.now()
-        self.upload_file_limit = args.upload_file_limit;
-        self.user_agent = args.user_agent;
-        self.download_file_list = args.download_file_list
+        self.upload_file_size_limit = int(config['UPLOAD']['FILE_SIZE_LIMIT']);
+        self.user_agent = config['DOWNLOAD']['USER_AGENT'];
+        self.download_file_list = config['DOWNLOAD']['FILE_LIST']
+        self.download_file_list_encoding = config['DOWNLOAD']['FILE_LIST_ENCODING']
+        self.tweet_format = config['TWEET']['FORMAT']
+        self.tweet_datefmt = config['TWEET']['DATEFMT']
     def downloadImage(self):
         # internet -> local
         dic = OrderedDict()
-        files = []
-        with open(self.download_file_list, 'r', encoding='utf-8') as fin:
+        with open(self.download_file_list, 'r', encoding=self.download_file_list_encoding) as fin:
             for line in fin:
                 text = line.rstrip('\n')
-                if len(text) == 0:
+                if text.startswith('http'):
+                    dic[text] = None 
+                else:
+                    logger.warning(text.encode(self.download_file_list_encoding))
                     continue
-                dic[text] = None
+                    
         if len(dic.keys()) == 0:
-            print('input:{0} Empty '.format(self.download_file_list))
+            logger.warning('input:{0} Empty '.format(self.download_file_list))
             return
         headers = {'User-Agent': self.user_agent}        
         for address in dic.keys():
-            print('download:{0}'.format(address))
+            logger.info('download:{0}'.format(address))
             r = requests.get(address, headers=headers)
             with open(os.path.join(self.upload, os.path.split(address)[1]), 'wb') as fout:
                 fout.write(r.content)
@@ -45,24 +62,27 @@ class tweetbot():
             for media in glob.iglob(os.path.join(self.upload, ext)):
                 # upload fileSize limit
                 size = os.path.getsize(media)
-                if size < self.upload_file_limit:
+                if size < self.upload_file_size_limit:
                     files.append(media)
                 else:
-                    print('skip:{0}, size:{1}'.format(media, size))
+                    logger.warning('skip:{0}, size:{1}'.format(media, size))
 
         return files
     def getFilePrefix(self, prefix='%Y%m%d%H%M_'):
         return self.dtNow.strftime(prefix)
     def tweet(self, media):
-        if self.t is None:
-            self.t = twitter.Api(consumer_key=self.args.consumer_key,
-                                 consumer_secret=self.args.consumer_secret,
-                                 access_token_key=self.args.access_token,
-                                 access_token_secret=self.args.access_token_secret)
-        text = '{0}\nFEZ 国家総力戦ランキング'.format(self.getFilePrefix('%Y/%m/%d %H:%M'))
-        media_id = self.t.UploadMediaSimple(media=media)
-        self.t.PostUpdate(status=text, media=media_id)
-
+        try:
+            if self.t is None:
+                self.t = twitter.Api(consumer_key=self.args.consumer_key,
+                                     consumer_secret=self.args.consumer_secret,
+                                     access_token_key=self.args.access_token,
+                                     access_token_secret=self.args.access_token_secret)
+            text = '{0}\n{1}'.format(self.getFilePrefix(self.tweet_datefmt), self.tweet_format)
+            media_id = self.t.UploadMediaSimple(media=media)
+            self.t.PostUpdate(status=text, media=media_id)
+            logger.info(text)
+        except Exception as ex:
+            logger.exception(ex)
     def backup(self, file):
         # file change & move
         try:
@@ -70,7 +90,9 @@ class tweetbot():
             os.rename(file, newFile)
             shutil.move(newFile, self.images)
         except Exception as ex:
-            print(ex)
+            logger.exception(ex)
+
+#if __name__ == "__main__":
 
 # parse
 parser = argparse.ArgumentParser(
@@ -80,16 +102,13 @@ parser.add_argument('--consumer_key', '-ck')
 parser.add_argument('--consumer_secret', '-cs')
 parser.add_argument('--access_token', '-at')
 parser.add_argument('--access_token_secret', '-ats')
-# optional
-parser.add_argument('--download_file_list', '-dl', default='./DownloadList.txt')
-parser.add_argument('--user_agent', '-ua', default='Mozilla/5.0 (compatible; bot/0.1; +https://twitter.com/fez_ranking_bot)')
-parser.add_argument('--upload_file_limit', '-limit', type=int, default=104448)
 
-print('')
+logger.info('')
 bot = tweetbot(parser.parse_args())
 bot.downloadImage()
 for media in bot.getImage():
-    print('tweet media:{0}'.format(media))
+    logger.info('tweet media:{0}'.format(media))
     bot.tweet(media)
     bot.backup(media)
     #break
+
