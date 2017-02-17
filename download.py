616bb837e5cc02ff2665e0cbec5f5e2fae762b03
umyuu <124dtiaka@gmail.com>
2017-02-17 17:17:40 +0900

    requestList yield
    requestList yield

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from logging import getLogger
 from collections import OrderedDict
 import os
+from pathlib import Path
 # library
 import requests
 
@@ -13,22 +15,27 @@ class download():
         self.file_list = config['DOWNLOAD']['FILE_LIST']
         self.file_list_encoding = config['DOWNLOAD']['FILE_LIST_ENCODING']
     def requestList(self):
-        dic = OrderedDict()
-        with open(self.file_list, 'r', encoding=self.file_list_encoding) as fin:
-            for line in fin:
+        """
+            @return yield  text
+        """
+        with open(self.file_list, 'r', encoding=self.file_list_encoding) as f:
+            for line in f:
                 text = line.rstrip('\n')
                 if not text.startswith('http'):
-                    logger.warning(text)
+                    #logger.warning(text)
                     continue
-                dic[text] = None
-        if len(dic) == 0:
-            logger.warning('input:{0} Empty '.format(self.file_list))
-        return dic
+                yield text
     def request(self):
-        # internet -> local
-        dic = self.requestList()
-        headers = {'User-Agent': self.user_agent}        
-        for address in dic.keys():
+        """
+           internet -> local
+           duplicate file 
+             address[_SequentialNo]
+        """
+        count = 0
+        headers = {'User-Agent': self.user_agent}
+        for address in self.requestList():
+            count += 1
+        #for address in dic.keys():
             logger.info('download:{0}'.format(address))
             r = requests.get(address, headers=headers)
             #filepath = os.path.join(self.data, os.path.basename(address))
@@ -38,8 +45,22 @@ class download():
             # Randomize duplicate path
             #while not os.path.isfile(filepath):                
             #    filepath += 'a'
-            #logger.info(filepath)                 
-            #with open(filepath, 'wb') as fout:
-            #    fout.write(r.content)
-            with open(os.path.join(self.data, os.path.basename(address)), 'wb') as fout:
+            #logger.info(filepath)
+            p = Path(self.data, os.path.basename(address))
+            i = 0;
+            s = '_'
+            while p.exists():
+                old = p
+                if not i == 0:
+                    s = ''
+                p =p.with_name('{0}{1}{2}{3}'.format(old.stem, s, str(i), old.suffix))
+                i += 1
+                logger.info("duplicate:")
+                logger.info(p)
+            
+            with p.open('wb') as fout:
                 fout.write(r.content)
+            #with open(os.path.join(self.data, os.path.basename(address)), 'wb') as fout:
+            #    fout.write(r.content)
+        if count == 0:
+            logger.warning('input:{0} Empty'.format(self.file_list))
