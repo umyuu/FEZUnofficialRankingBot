2a5480b848f52c4d47edace31385c701fcb8a3b7
umyuu <124dtiaka@gmail.com>
2017-02-17 12:14:06 +0900

    skip bom
    1.fileskip bom utf-8-sig
    2.add method.
    tweetbot#twitter_init
    tweetbot#deletetweet
    3.add_argument
    debug
    version

--- TweetBot.py
+++ TweetBot.py
@@ -21,7 +21,7 @@ logger.addHandler(handler)
 class tweetbot():
     def __init__(self, args):
         self.args = args;
-        self.t = None;
+        self.api = None;
         self.download = download.download(config)
         self.upload = config['WORK_FOLDER']['UPLOAD']
         self.images = config['WORK_FOLDER']['images']
@@ -29,6 +29,12 @@ class tweetbot():
         self.upload_max_file_size = int(config['UPLOAD']['MAX_FILESIZE']);
         self.tweet_format = config['TWEET']['FORMAT']
         self.tweet_datefmt = config['TWEET']['DATEFMT']
+    def twitter_init(self):
+        if self.api is None:
+            self.api = twitter.Api(consumer_key=self.args.consumer_key,
+                                 consumer_secret=self.args.consumer_secret,
+                                 access_token_key=self.args.access_token,
+                                 access_token_secret=self.args.access_token_secret)
     def downloadImage(self):
         # internet -> local
         self.download.request()
@@ -49,15 +55,24 @@ class tweetbot():
         return self.dtNow.strftime(prefix)
     def tweet(self, media):
         try:
-            if self.t is None:
-                self.t = twitter.Api(consumer_key=self.args.consumer_key,
-                                     consumer_secret=self.args.consumer_secret,
-                                     access_token_key=self.args.access_token,
-                                     access_token_secret=self.args.access_token_secret)
+            self.twitter_init()
             text = '{0}\n{1}'.format(self.getFilePrefix(self.tweet_datefmt), self.tweet_format)
-            #media_id = self.t.UploadMediaSimple(media=media)
-            #self.t.PostUpdate(status=text, media=media_id)
+            #media_id = self.api.UploadMediaSimple(media=media)
+            #self.api.PostUpdate(status=text, media=media_id)
             logger.info(text)
+        except Exception as ex:
+            logger.exception(ex)
+    def deletetweet(self):
+        """
+            @params none
+        """
+        try:
+            self.twitter_init()
+            screen_name = 'fez_ranking_bot'
+            limit = 1
+            for s in self.api.GetUserTimeline(screen_name=screen_name, count=limit):
+                self.api.DestroyStatus(s.id)
+                logger.info('delete:{0},posted:{1}'.format(s.text, s.created_at))
         except Exception as ex:
             logger.exception(ex)
     def backup(self, file):
@@ -75,10 +90,12 @@ class tweetbot():
 parser = argparse.ArgumentParser(
     description='FEZ National Total War Ranking')
 # must twitter auth params
-parser.add_argument('--consumer_key', '-ck')
-parser.add_argument('--consumer_secret', '-cs')
-parser.add_argument('--access_token', '-at')
-parser.add_argument('--access_token_secret', '-ats')
+parser.add_argument('--version', action='version', version='version 0.0.1')
+parser.add_argument('--consumer_key', '-ck', required=True, help='Twitter Apps Auth set consumer_key')
+parser.add_argument('--consumer_secret', '-cs', required=True, help='Twitter Apps Auth set consumer_secret')
+parser.add_argument('--access_token', '-at', required=True, help='Twitter Apps Auth set access_token')
+parser.add_argument('--access_token_secret', '-ats', required=True, help='Twitter Apps Auth set access_token_secret')
+parser.add_argument('--debug', action='store_true', default=True)
 
 logger.info('')
 bot = tweetbot(parser.parse_args())
@@ -86,5 +103,6 @@ bot.downloadImage()
 for media in bot.getImage():
     logger.info('tweet media:{0}'.format(media))
     bot.tweet(media)
+    #bot.deletetweet()
     bot.backup(media)
     #break
