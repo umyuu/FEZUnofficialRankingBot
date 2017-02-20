7c1ef14dfa03c821670b94c345a0645cc2058d7e
umyuu <124dtiaka@gmail.com>
2017-02-20 19:49:40 +0900

    Replace print with logging
    Replace print with logging

--- country.py
+++ country.py
@@ -1,33 +1,45 @@
 # -*- coding: utf-8 -*-
-from logging import getLogger
+from logging import getLogger, StreamHandler, DEBUG
 import configparser
 import os
 import sys
 import glob
 from collections import OrderedDict
 import functools
+# library
 import cv2
 import numpy as np
 logger = getLogger('myapp.tweetbot')
+if __name__ == "__main__":
+    handler = StreamHandler()
+    handler.setLevel(DEBUG)
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler)
 
 class DataProcessor():
     def __init__(self, name):
         self._name = name
         self.color = None
     def prepare(self):
+        """
+            @return color image
+        """
         self.color = self.__read(self._name)
         if self.color is None:
             return None
         return self.color
-    def batch(self):
+    def batch(self,ranking=1):
+        """
+            @return binary image
+        """
         # 画像の白文字部分を抽出
         white = self.__getWhiteMasking(self.color.copy())
         binary = self.__binary_threshold(self.color)
         # マスク
-        binary = cv2.bitwise_and(binary, binary, mask= white)
-        height, width = self.color.shape[:2]
+        binary = cv2.bitwise_and(binary, binary, mask=white)
         # １位より下を黒色で塗りつぶしてマスク。
-        cv2.rectangle(binary, (0, min(500, height)),(width,height),(0,0,0),-1)
+        height, width = self.color.shape[:2]
+        cv2.rectangle(binary, (0, min(500, height)), (width,height), (0,0,0), -1)
         return binary
     def __getWhiteMasking(self, img):
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -48,8 +60,9 @@ class DataProcessor():
     def __read(self, name, zoom=2):
         color = cv2.imread(name)
         if color is None:
-            print("notfound:{0}".format(name))
+            logger.info("notfound:{0}".format(name))
             return None
+        # note:cv2.resize remove must #batch change
         return cv2.resize(color, (color.shape[1]*zoom, color.shape[0]*zoom), interpolation=cv2.INTER_CUBIC)
 class country():
     def __init__(self, config):
@@ -63,6 +76,11 @@ class country():
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.detector = cv2.AKAZE_create()
     def __Detection(self, first):
+        """
+            @return keys        basename
+                    value       sum(distance)
+                    order by    value
+        """
         (kp_first, des_first) = self.detector.detectAndCompute(first, None)
         d = dict()
         for media in glob.iglob(os.path.join(self.hints, "*.png")):
@@ -70,8 +88,8 @@ class country():
             try:
                 (kp_second, des_second) = self.__cachedetect(media)
                 dist = [m.distance for m in self.bf.match(des_first, des_second)]
-                result = sum(dist) / len(dist)
-                d[basename] = result
+                #dist = [m.distance for m in self.bf.knnMatch(des_first, des_second, 3)]
+                d[basename] = sum(dist) / len(dist)
             except cv2.error:
                 d[basename] = sys.maxsize
 
@@ -84,15 +102,19 @@ class country():
         #cv2.imwrite('./binary_{0}.png'.format(os.path.basename(media)), pro.batch())
         return self.detector.detectAndCompute(pro.batch(), None)
     def getCountry(self, src):
+        """
+            @return country list
+        """
         pro = DataProcessor(src)
         if pro.prepare() is None:
-           return
+           logger.error('image error:{0}'.format(src))
+           return None
         binary = pro.batch()
         # imwrite#cvtColorで色情報が足りないとエラー
         #cv2.imwrite('./b_color.png', pro.color)
         #cv2.imwrite('./b_binary.png', binary)
         d = self.__Detection(binary)
-        print(d)
+        logger.info(d)
         return d
     def getName(self, n):
         return self.names[n]
@@ -102,15 +124,19 @@ def main():
     with open('./setting.ini', 'r', encoding='utf-8-sig') as f:
         config.read_file(f)
     c = country(config)
-    #d = c.getCountry('./backup/hints/Hordine.png')
+    # benchMark
     for i in range(1):
-        d = c.getCountry('./backup/hints/201702190825_0565e4fcbc166f00577cbd1f9a76f8c7.png')
-        d = c.getCountry('./backup/test/201702191909_ac08ccbbb04f2a1feeb4f8aaa08ae008.png')
-        d = c.getCountry('./backup/test/201702192006_2e268d7508c20aa00b22dfd41639d65e.png')
-        for k in d.keys():
-            country_name = c.getName(k)
-            print(country_name)
-            break;
-    
+        ele = ['./backup/hints/201702190825_0565e4fcbc166f00577cbd1f9a76f8c7.png',
+             './backup/test/201702191909_ac08ccbbb04f2a1feeb4f8aaa08ae008.png',
+             './backup/test/201702192006_2e268d7508c20aa00b22dfd41639d65e.png',
+             ]
+        for l in ele:
+            logger.info(l)
+            d = c.getCountry(l)
+            for k in d.keys():
+                country_name = c.getName(k)
+                logger.info(country_name)
+                break;
+        
 if __name__ == "__main__":
     main()
