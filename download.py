b6eac5fe437886e8fe169d17c55590d531fd6ea2
umyuu <124dtiaka@gmail.com>
2017-02-21 01:00:06 +0900

    Clean ContentType PHASE 2
    Clean ContentType PHASE 2

--- download.py
+++ download.py
@@ -18,6 +18,7 @@ class download():
         self.file_list = config['DOWNLOAD']['FILE_LIST']
         self.file_list_encoding = config['DOWNLOAD']['FILE_LIST_ENCODING']
         self.comp = re.compile(r'/(\w+);?')
+        self.htmllink = None
     def requestList(self):
         """
             @yield URL
@@ -28,6 +29,10 @@ class download():
                 if not text.startswith('http'):
                     #logger.warning(text)
                     continue
+                if self.htmllink is not None:
+                    link = self.htmllink
+                    self.htmllink = None;
+                    yield link
                 yield text
     @functools.lru_cache(maxsize=4)
     def getSuffix(self, contentType, suffix='.html'):
@@ -41,40 +46,50 @@ class download():
             return '.' + m.group(1)
         logger.error(contentType)
         return suffix
+    def parselink(self):
+        return None
     def request(self):
         """
            internet -- (Get) --> local
-           exsample)exists file
-             exsample.png
-             exsample(1).png
-             exsample(2).png
-             exsample(n).png
         """
         count = 0
         headers = {'User-Agent': self.user_agent}
         for address in self.requestList():
             count += 1
-            logger.info('download:{0}'.format(address))
-            basename = os.path.basename(address)
-            r = requests.get(address, headers=headers)
-            contentType = r.headers['content-type']
-            logger.info('content-type:{0},decode:{1}'.format(contentType, self.getSuffix(contentType)))
-            
-            with tempfile.NamedTemporaryFile(dir=self.data, delete=False) as temp:
-                temp.write(r.content)
-                temp_file_name = temp.name
-                if len(basename) == 0:
-                    logger.warning('create_filename:{0}'.format(os.path.basename(temp.name)))
-                    basename = os.path.basename(temp_file_name) + self.getSuffix(contentType)
-                p = Path(self.data, basename)
-                i = 0;
-                basePath = p
-                while p.exists():
-                    i += 1
-                    p = p.with_name('{0}({1}){2}'.format(basePath.stem, i, basePath.suffix))
-                    if sys.maxsize == i:
-                        break
-            
-            os.replace(temp_file_name, str(p))
+            #if contentType.startswith('image'):
+            self.get(address, headers)
+
         if count == 0:
             logger.warning('input:{0} Empty'.format(self.file_list))
+    def sequential(self, file):
+        """
+            exsample)exists file
+                exsample.png
+                exsample(1).png
+                exsample(2).png
+                exsample(n).png
+        """
+        i = 0;
+        basePath = file
+        while file.exists():
+            i += 1
+            file = file.with_name('{0}({1}){2}'.format(basePath.stem, i, basePath.suffix))
+            if sys.maxsize == i:
+                break
+        return file
+    def get(self, address, headers):
+        logger.info('download:{0}'.format(address))
+        basename = os.path.basename(address)
+        r = requests.get(address, headers=headers)
+        contentType = r.headers['content-type']
+        logger.info('content-type:{0},decode:{1}'.format(contentType, self.getSuffix(contentType)))
+        with tempfile.NamedTemporaryFile(dir=self.data, delete=False) as temp:
+            temp.write(r.content)
+            temp_file_name = temp.name
+            if len(basename) == 0:
+                logger.warning('create_filename:{0}'.format(os.path.basename(temp.name)))
+                basename = os.path.basename(temp_file_name) + self.getSuffix(contentType)
+            p = Path(self.data, basename)
+            p = self.sequential(p)
+            
+        os.replace(temp_file_name, str(p))
