aaa77317912625d569822db8265e74e4b8f70e1b
umyuu <124dtiaka@gmail.com>
2017-03-04 17:55:57 +0900

    Resources FileFound Test
    Resources FileFound Test

--- test/test_country.py
+++ test/test_country.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
-import configparser
 import pytest
 
-from src import hsvcolor
+from src import TweetBot
 
 class TestClass(object):
-    def test_load(self):
-        config = configparser.ConfigParser()
-        with open('../resource/setting.ini', 'r', encoding='utf-8-sig') as f:
-                config.read_file(f)
-        c = hsvcolor.HSVcolor(0, 0, 0)
-        assert c is not None
+    def test_loadSettingFile(self):
+        config = TweetBot.loadConfig('../resource/setting.ini')
+        twitter_auth = TweetBot.loadConfig(config['AUTH']['TWITTER'])
 
 if __name__ == '__main__':
     pytest.main("--capture=sys")
