c7be04853dccef794131ad360e7d2d0f49ad8623
umyuu <124dtiaka@gmail.com>
2017-03-05 01:00:52 +0900

    run script python implements
    run script python implements

---
+++ tweet_win.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+    run script tweetbot.py 
+"""
+import os
+import subprocess
+
+class RCD(object):
+    """
+        with statement.
+        implements Restore　Current Diretory.
+    """
+    def __init__(self):
+        self.__chdir = None
+    @property
+    def chdir(self):
+        """
+            @return chdir
+        """
+        return self.__chdir
+    def __enter__(self):
+        self.__chdir = os.getcwd()
+        return self
+    def __exit__(self, exception_type, exception_value, traceback):
+        os.chdir(self.__chdir)
+class Alarm(object):
+    """
+        alarm event implements by Windows
+    """
+    def __init__(self, timeout):
+        self.timeout = timeout
+    def wait(self):
+        """
+            windows command timeout /T
+        """
+        subprocess.call('timeout /T {0}'.format(self.timeout))
+        return self
+
+if __name__ == "__main__":
+    with RCD():
+        os.chdir(os.path.join(os.getcwd(), 'src'))
+        Alarm(15).wait()
+        try:
+            subprocess.call('python tweetbot.py')
+        except KeyboardInterrupt as ex:
+            print(ex)
+        Alarm(60).wait()
--- □tweet.bat
+++
@@ -1,5 +0,0 @@
-REM Windows Bat File
-timeout 15
-cd src
-python TweetBot.py
-timeout 60
