53cdc85572a43dea7e1b14da5ea952ff51349b87
umyuu <124dtiaka@gmail.com>
2017-03-09 21:02:01 +0900

    remake ImageStream
    ImageStream#callrable

--- tool/imagefillter_test.py
+++ tool/imagefillter_test.py
@@ -1,90 +1,52 @@
 # -*- coding: utf-8 -*-
 import argparse
-from abc import ABCMeta, abstractmethod
 # library
 import cv2
 
-class IImageFilter(metaclass=ABCMeta):
-    def __init__(self, name='IImageFilter'):
-        self.name = name
-    @abstractmethod
-    def filtered(self, stream):
+class ImageStream(object):
+    def __init__(self, preprocessor=None, task=None, finish=None):
+        if preprocessor is None:
+            preprocessor = self.Empty
+        if task is None:
+            task = self.Empty
+        if finish is None:
+            finish = self.Empty
+        self.preprocessor = preprocessor
+        self.task = task
+        self.finish = finish
+    def Empty(self, stream):
         """
-            @params stream source stream
-            @return filtered stream
+            stream => result
+            @param {stream}  stream
+            @return {stream} stream
         """
         return stream
-class EmptyFilter(IImageFilter):
-    def __init__(self):
-        super().__init__('EmptyFilter')
-    def filtered(self, stream):
+    def transform(self, data):
+        for t in [self.preprocessor, self.task, self.finish]:
+            data = t(data)
+            cv2.imshow('image:', data)
+            cv2.waitKey(3000)
+        return data
+class Processor(object):
+    def prepare(self, stream):
+        assert stream is not None
         return stream
-class GrayScaleFilter(IImageFilter):
-    def __init__(self):
-        super().__init__('GrayScaleFilter')
-    def filtered(self, stream):
-        result = cv2.cvtColor(stream, cv2.COLOR_BGR2GRAY)
-        return result
-class AdaptiveThresholdFilter(IImageFilter):
-    def __init__(self):
-        super().__init__('AdaptiveThresholdFilter')
-    def filtered(self, stream):
-        result = cv2.adaptiveThreshold(stream
+    def task(self, stream):
+        """
+            filtered prepare
+            @param {image}  stream
+            @return {image} filtered
+        """
+        result = stream
+        result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+        result = cv2.adaptiveThreshold(result
                     , 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 3)
         return result
-class CanvesFillFilter(IImageFilter):
-    def __init__(self):
-        super().__init__('CanvesFillFilter')
-    def filtered(self, stream):
+    def finish(self, stream):
         height, width = stream.shape[:2]
-        cv2.rectangle(stream, (0, min(self.widthLimit, height)), (width,height), (0,0,0), -1)
+        widthLimit = 400
+        cv2.rectangle(stream, (0, min(widthLimit, height)), (width, height), (0 , 0, 0), -1)
         return stream
-class IregalClassFilter(object):
-    def __init__(self):
-        pass
-class ImageStream(object):
-    __slots__ = ['filters','data']
-    def __init__(self):
-        self.filters = []
-        self.data = None
-    def addFilter(self, f):
-        if not isinstance(f, IImageFilter):
-            assert False, 'non implements'
-        if not (f in self.filters):
-            self.filters.append(f)
-        return self
-    def removeFilter(self, f):
-        if not isinstance(f, IImageFilter):
-            assert False, 'non implements'
-        if f in self.filters:
-            self.filters.remove(f)
-        return self
-    def clearFilter(self):
-        self.filters = []
-        return self
-    def tofiltered(self):
-        for f in self.filters:
-            self.data = f.filtered(self.data)
-            assert self.data is not None, 'filtered result None'
-            self.fire_onfiltered(f)
-        return self.data
-    def fire_onfiltered(self, sender):
-        """
-            filtered event
-            @params sender IImageFilter implemts class
-        """
-        print(sender.name + ':event')
-        cv2.imshow('image:', self.data)
-        cv2.waitKey(3000)
-        pass
-def test_scenario1():
-    stream = ImageStream()
-    stream.addFilter(IregalClassFilter())
-def test_scenario2():
-    stream = ImageStream()
-    empty = EmptyFilter()
-    stream.addFilter(empty)
-    stream.addFilter(empty)
 def main():
     parser = argparse.ArgumentParser(prog='ImageFilter',
                                      description='ImageFilter Simulator')
@@ -93,21 +55,11 @@ def main():
     args = parser.parse_args()
     print('args:{0}'.format(args))
     
-    stream = ImageStream()
-    stream.addFilter(EmptyFilter())
-    stream.addFilter(GrayScaleFilter())
-    stream.addFilter(AdaptiveThresholdFilter())
-    # 
-    canvs = CanvesFillFilter()
-    canvs.widthLimit = 400
-    stream.addFilter(canvs)
- 
-    stream.data = cv2.imread(args.image)
-    assert stream.data is not None
-    cv2.imshow('image:', stream.data)
-    cv2.waitKey(3000)
-    # fillter処理
-    stream.tofiltered()
+    p = Processor()
+    stream = ImageStream(preprocessor=p.prepare, task=p.task, finish=p.finish)
+    data = cv2.imread(args.image)
+    #
+    stream.transform(data)
 
 if __name__ == "__main__":
     main()
