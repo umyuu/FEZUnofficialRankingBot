d1648d0317a0278bc79ccb86f0c44e22848095b2
umyuu <124dtiaka@gmail.com>
2017-03-09 21:15:26 +0900

    pylint
    pylint

--- tool/imagefillter_test.py
+++ tool/imagefillter_test.py
@@ -2,8 +2,12 @@
 import argparse
 # library
 import cv2
+# pylint: disable=C0103
 
 class ImageStream(object):
+    """
+        ImageStream stream transform
+    """
     def __init__(self, preprocessor=None, task=None, finish=None):
         if preprocessor is None:
             preprocessor = self.Empty
@@ -22,20 +26,34 @@ class ImageStream(object):
         """
         return stream
     def transform(self, data):
+        """
+            call
+                preprocessor => task => finish
+            @param {stream}  data
+            @return {stream} dara
+        """
         for t in [self.preprocessor, self.task, self.finish]:
             data = t(data)
             cv2.imshow('image:', data)
             cv2.waitKey(3000)
         return data
 class Processor(object):
+    """
+        ImageStream caller Processor method.
+    """
     def prepare(self, stream):
+        """
+            filtered prepare
+            @param {stream}  stream
+            @return {stream} prepared stream
+        """
         assert stream is not None
         return stream
     def task(self, stream):
         """
-            filtered prepare
-            @param {image}  stream
-            @return {image} filtered
+            filtered task
+            @param {stream}  stream
+            @return {stream} filtered
         """
         result = stream
         result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
@@ -43,9 +61,14 @@ class Processor(object):
                     , 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 3)
         return result
     def finish(self, stream):
+        """
+            filtered finish
+            @param {stream}  stream
+            @return {stream} finish stream
+        """
         height, width = stream.shape[:2]
         widthLimit = 400
-        cv2.rectangle(stream, (0, min(widthLimit, height)), (width, height), (0 , 0, 0), -1)
+        cv2.rectangle(stream, (0, min(widthLimit, height)), (width, height), (0, 0, 0), -1)
         return stream
 def main():
     parser = argparse.ArgumentParser(prog='ImageFilter',
@@ -54,7 +77,7 @@ def main():
     parser.add_argument('--image', '-in', default='../resource/Netzawar.png')
     args = parser.parse_args()
     print('args:{0}'.format(args))
-    
+
     p = Processor()
     stream = ImageStream(preprocessor=p.prepare, task=p.task, finish=p.finish)
     data = cv2.imread(args.image)
